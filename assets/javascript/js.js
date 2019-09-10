3f711c9f2e420336cde09b263835008bacb87bf4
HowardKuo <howardkuo91@gmail.com>
2019-09-10 00:43:22 -0700

    fixed buttons and added descriptions

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,29 +1,35 @@
 $(document).ready(function(){
     $('.carousel').carousel({
-        indicators: true
+        indicators: true,
+        onCycleTo: function(data) {
+            const index = $(data).index();
+            let description;
+            switch (index)  {
+                case 0:
+                    description = '<h4>GitHub Repositries</h4><br><p>A list of all of my GitHub Repositries.</p>';
+                    $('.panel-description').html(description);
+                    break;
+                case 1:
+                    description = '<h4>Pokemon Guess</h4><br><p>A Pokemon themed Hangman game. Built with HTML, CSS, and JavaScript</p>';
+                    $('.panel-description').html(description);
+                    break;
+                case 2:
+                    description = '<h4>Crystals Collector</h4><br><p>A math game where you try to match the number given with 4 random unknown numbers. Built with HTML, CSS, and JavaScript</p>';
+                    $('.panel-description').html(description);
+                    break;
+                case 3:
+                    description = '<h4>Train Schedule</h4><br><p>A train scheduling application that takes in user inputs and adds them to a list of trains. Built with HTML, CSS, and JavaScript. </p>';
+                    $('.panel-description').html(description);
+                    break;
+            };
+            
+         }
     });
 });
-const description = 'something will show here';
-let id;
-$('.active img').click(function(){
-    id = $('.active').attr('id');
-    console.log('click: ',id)
-});
-console.log(id);
-// switch (panelID)  {
-//     case 0:
-//         description = "panel1";
-//         break;
-//     case 1:
-//         description = "panel2";
-//         break;
-//     case 2:
-//         description = "panel3";
-//         break;
-//     case 3:
-//         description = "panel4";
-// };
-$('.panel-description').append(description);
+
+
+
+
 
 // move next carousel
 $('.moveNextCarousel').click(function(e){
@@ -37,9 +43,4 @@ $('.movePrevCarousel').click(function(e){
     e.preventDefault();
     e.stopPropagation();
     $('.carousel').carousel('prev');
-});
-
-$('.carosel').click(function(e) {
-    e.preventDefault();
-    e.stopPropagation();
 });
